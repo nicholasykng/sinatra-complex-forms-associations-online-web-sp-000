404a9994af4b7e82e7d3dff7f26162b1279d3cc8
Nicholas Ng <nicholasykng@gmail.com>
2020-01-08 03:29:19 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,7 +26,7 @@ class PetsController < ApplicationController
 
   get "/pets/:id/edit" do
     @pet = Pet.find(params[:id])
-    erb "/pets/edit"
+    erb :"/pets/edit"
   end
 
   patch '/pets/:id' do
